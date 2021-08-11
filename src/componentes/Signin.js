d1a5b3713d7cc520dc06760c89eec1e1d233b01d
Kelvin Hernandez <youmater2001@gmail.com>
2021-08-11 13:10:54 -0400

    Cambios leves

--- src/componentes/Signin.js
+++ src/componentes/Signin.js
@@ -1,19 +1,33 @@
 import React, { useRef } from 'react'
-import { auth } from '../firebase';
+import { db,auth, crearUsuario } from '../firebase';
 import './Signin.css'
 const Signin = () => {
+    const nombreref = useRef(null);
+    const apellidoref = useRef(null);
     const emailref = useRef(null);
     const passwordref = useRef(null);
-    const signUp = e => {
+    const signUp = async (e) => {
         e.preventDefault();
+        try{
         auth.createUserWithEmailAndPassword(
             emailref.current.value,
             passwordref.current.value
-        ).then(user =>{
-            console.log(user)
-        }).catch(err =>{
-            console.log(err);
-        })
+        ).then(credenciales =>{
+                db.collection("usuarios").doc(credenciales.user.uid).set({
+                nombre: nombreref,
+                apellido: apellidoref,
+                correo: emailref
+            });
+        }).then(()=>{
+            console.log("fire store va bien");
+        });
+
+
+
+        }
+        catch(error){
+            console.log(error);
+        }
     }
     const signIn = e => {
         e.preventDefault();
@@ -30,8 +44,8 @@ const Signin = () => {
         <div className="signin">
             <form action="">
                 <h1>Log in</h1>
-                <input type="name" placeholder="Nombre"/>
-                <input type="lastname" placeholder="Apellido"/>
+                <input ref={nombreref} type="name" placeholder="Nombre"/>
+                <input ref={apellidoref}  type="lastname" placeholder="Apellido"/>
                 <input ref={emailref} type="email" placeholder="Correo electrónico"/>
                 <input ref={passwordref} type="password" placeholder="Contraseña"/>
                 <button onClick = {signIn}>Log in</button>
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,41 @@
 import firebase from 'firebase';
+import 'firebase/firestore';
+
 var firebaseConfig = {
     apiKey: "AIzaSyB6gw_7_9VKNE_HtRIap5SxaBG6OE5_jlc",
     authDomain: "proyecto-final-muro-kelvin.firebaseapp.com",
+    databaseURL: "https://proyecto-final-muro-kelvin-default-rtdb.firebaseio.com",
     projectId: "proyecto-final-muro-kelvin",
     storageBucket: "proyecto-final-muro-kelvin.appspot.com",
     messagingSenderId: "951455340192",
     appId: "1:951455340192:web:d5de7be8c5dbf751e6dbc5"
   };
   // Initialize Firebase
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
-  const db = firebase.firestore();
+  export const db = firebase.firestore();
   const auth = firebase.auth();
   export{auth};
-  export default db;
+  export const crearUsuario= async (usuario, Nombre, Apellido) => {
+    if (!usuario) return;
+
+    const userRef = db.doc(`usuarios/${usuario.uid}`);
+    const snapshot = userRef.get();
+
+    if (!snapshot.exists){
+        const {apellido} = Apellido;
+        const {correo} = usuario;
+        const {nombre} = Nombre;
+
+
+    try{
+        userRef.set({
+            nombre,
+            apellido,
+            correo
+        });
+    }catch(error){
+        console.log('Error creando el usuario', error);
+    }
+    }
+  };
